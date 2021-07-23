42ef7b6dda57d2798cc3ca4a28ffebf0b8a424ec
DESKTOP-SKERJDK\saloo <oleg.d.salo@gmail.com>
2021-07-23 22:32:05 +0300

    delete app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import axios from 'axios';
 function App() {
   // NODE_URL => http://localhost:${port (from nodejs)}
   // let port = 3000;
+
   axios.get(`http://localhost:3000 `)
     .then(function (response) {
       // handle success
       console.log(response);
+      console.log(response.data);
     })
     .catch(function (error) {
       // handle error
       console.log(error);
     })
-    .then(function () {
+    .then(function (data) {
       // always executed
+
     });
+
   return (
     <>
     </>
