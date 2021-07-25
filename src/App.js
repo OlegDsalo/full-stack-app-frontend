a336d7484ac7a72e4bb0d3f2119305de8664092e
DESKTOP-SKERJDK\saloo <oleg.d.salo@gmail.com>
2021-07-25 16:59:08 +0300

    add state and effect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import './App.css';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
-  // NODE_URL => http://localhost:${port (from nodejs)}
-  // let port = 3000;
 
-  axios.get(`http://localhost:3000 `)
-    .then(function (response) {
-      // handle success
-      console.log(response);
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function (data) {
-      // always executed
+  const [outresponse, setResponse] = useState(null);
+  useEffect(() => {
 
-    });
+    // fetch("http://localhost:3000")
+    //   .then(response => response.json())
+    //   .then(data => setResponse(data))
+
+    axios.get("http://localhost:3000")
+      .then(function (response) {
+        // handle success
+        // console.log(response);
+        // console.log();
+        setResponse(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function (data) {
+        // always executed
+
+      });
+  });
 
   return (
     <>
+      {outresponse}
     </>
 
   );
